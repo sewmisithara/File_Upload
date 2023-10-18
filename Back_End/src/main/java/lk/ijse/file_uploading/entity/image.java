7c620b3f9bbd3e0f1dd811958558d18c3b21653c
sewmisithara <sewmisithara23520@gmail.com>
2023-10-18 23:27:50 +0530

    update image.java

--- Back_End/src/main/java/lk/ijse/file_uploading/entity/image.java
+++ Back_End/src/main/java/lk/ijse/file_uploading/entity/image.java
@@ -1,9 +1,19 @@
 package lk.ijse.file_uploading.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class image {
 
     @Id
