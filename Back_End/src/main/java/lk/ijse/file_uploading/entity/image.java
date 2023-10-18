56649e394c586002a629dd2dd289f54e9f19b8c2
sewmisithara <sewmisithara23520@gmail.com>
2023-10-18 23:26:48 +0530

    add image

--- Back_End/src/main/java/lk/ijse/file_uploading/entity/image.java
+++ Back_End/src/main/java/lk/ijse/file_uploading/entity/image.java
@@ -1,4 +1,17 @@
 package lk.ijse.file_uploading.entity;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 public class image {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String fileString;
+
+    public Image(String fileString) {
+        this.fileString = fileString;
+    }
 }
