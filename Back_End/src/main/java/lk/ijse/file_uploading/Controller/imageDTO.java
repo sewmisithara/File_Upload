46c0a9ccf0fb2a1fe2c6dfebe288ad2072a45a78
sewmisithara <sewmisithara23520@gmail.com>
2023-10-18 23:24:26 +0530

    update imageDTO

--- Back_End/src/main/java/lk/ijse/file_uploading/Controller/imageDTO.java
+++ Back_End/src/main/java/lk/ijse/file_uploading/Controller/imageDTO.java
@@ -1,6 +1,15 @@
 package lk.ijse.file_uploading.Controller;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 import org.springframework.web.multipart.MultipartFile;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 
 public class imageDTO {
     private String id;
