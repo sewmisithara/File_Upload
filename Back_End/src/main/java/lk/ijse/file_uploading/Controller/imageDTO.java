336b94263f0a15aab7714dfa609b9c357fef9724
sewmisithara <sewmisithara23520@gmail.com>
2023-10-18 23:23:17 +0530

    crate FileUploadController

--- Back_End/src/main/java/lk/ijse/file_uploading/Controller/imageDTO.java
+++ Back_End/src/main/java/lk/ijse/file_uploading/Controller/imageDTO.java
@@ -1,4 +1,12 @@
 package lk.ijse.file_uploading.Controller;
 
+import org.springframework.web.multipart.MultipartFile;
+
 public class imageDTO {
+    private String id;
+    private MultipartFile fileString;
+
+    public ImageDTO(MultipartFile fileString) {
+        this.fileString = fileString;
+    }
 }
