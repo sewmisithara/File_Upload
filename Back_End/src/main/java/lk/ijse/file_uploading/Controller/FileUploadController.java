d400ae899e3b20f0b5632a83a443794ab1f88870
sewmisithara <sewmisithara23520@gmail.com>
2023-10-18 19:34:59 +0530

    update FileUploadController

--- Back_End/src/main/java/lk/ijse/file_uploading/Controller/FileUploadController.java
+++ Back_End/src/main/java/lk/ijse/file_uploading/Controller/FileUploadController.java
@@ -1,4 +1,25 @@
 package lk.ijse.file_uploading.Controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
 public class FileUploadController {
+    @Autowired
+    private FileUploadService fileRepo;
+
+    @PostMapping
+    public ResponseUtil uploadFileWithSpringWay(@ModelAttribute ImageDTO dto) {
+        fileRepo.saveFileLocation(dto);
+        return new ResponseUtil("ok","Successfuly",null);
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @GetMapping(path = "/lastImageLoad")
+    public ResponseUtil getAllDriver() {
+        return new ResponseUtil("OK", "Successfully Loaded. :", fileRepo.getLastImageLocation());
+    }
 }
